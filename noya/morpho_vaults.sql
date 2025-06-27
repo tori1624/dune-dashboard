10cffdfa151185badd20889b46f060a3b6bb6664
Youngho Lee <emfo0124@naver.com>
2025-06-27 14:40:42 +0900

    feat: add TVL data

--- noya/morpho_vaults.sql
+++ noya/morpho_vaults.sql
@@ -1,3 +1,13 @@
+with eth_price as (
+    select date_trunc('day', minute) as dt
+         , avg(price) as price
+    from prices.usd
+    where symbol = 'ETH'
+    and contract_address is null
+    group by 1
+    having date_trunc('day', minute) >= date '2025-06-05'
+)
+
 select *
      , bnmorpho_usd/nmorpho_usd as nu_bonding_ratio
      , bnmorpho_eth/nmorpho_eth as ne_bonding_ratio
@@ -10,6 +20,10 @@ select *
          else (nmorpho_usd_loop/100000)
        end as nul_cap_ratio
      , nmorpho_eth_loop/40 as nel_cap_ratio
+     , nmorpho_eth*e.price as ne_tvl
+     , nmorpho_eth_loop*e.price as nel_tvl
+     , nmorpho_usd+nmorpho_usd_loop+(nmorpho_eth*e.price)+(nmorpho_eth_loop*e.price) as total_tvl
 from query_5346169
+left join eth_price e using(dt)
 order by dt desc
 ;
