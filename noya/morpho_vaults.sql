d7cb5ec866d298218245d006700382532d2835c7
Youngho Lee <emfo0124@naver.com>
2025-06-30 21:57:23 +0900

    style: format ratio values as percentages

--- noya/morpho_vaults.sql
+++ noya/morpho_vaults.sql
@@ -9,17 +9,17 @@ with eth_price as (
 )
 
 select *
-     , bnmorpho_usd/nmorpho_usd as nu_bonding_ratio
-     , bnmorpho_eth/nmorpho_eth as ne_bonding_ratio
-     , bnmorpho_usd_loop/nmorpho_usd_loop as nul_bonding_ratio
-     , bnmorpho_eth_loop/nmorpho_eth_loop as nel_bonding_ratio
-     , nmorpho_usd/1000000 as nu_cap_ratio
-     , nmorpho_eth/400 as ne_cap_ratio
+     , (bnmorpho_usd/nmorpho_usd)*100 as nu_bonding_ratio
+     , (bnmorpho_eth/nmorpho_eth)*100 as ne_bonding_ratio
+     , (bnmorpho_usd_loop/nmorpho_usd_loop)*100 as nul_bonding_ratio
+     , (bnmorpho_eth_loop/nmorpho_eth_loop)*100 as nel_bonding_ratio
+     , (nmorpho_usd/1000000)*100 as nu_cap_ratio
+     , (nmorpho_eth/400)*100 as ne_cap_ratio
      , case
-         when (nmorpho_usd_loop/100000) > 1 then 1
-         else (nmorpho_usd_loop/100000)
+         when (nmorpho_usd_loop/100000) > 1 then 100
+         else (nmorpho_usd_loop/100000)*100
        end as nul_cap_ratio
-     , nmorpho_eth_loop/40 as nel_cap_ratio
+     , (nmorpho_eth_loop/40)*100 as nel_cap_ratio
      , nmorpho_eth*e.price as ne_tvl
      , nmorpho_eth_loop*e.price as nel_tvl
      , nmorpho_usd+nmorpho_usd_loop+(nmorpho_eth*e.price)+(nmorpho_eth_loop*e.price) as total_tvl
