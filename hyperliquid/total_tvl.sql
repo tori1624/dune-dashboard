4bc5e436b01a50cb95c8e494bd2a806ba3293416
Youngho Lee <emfo0124@naver.com>
2025-07-15 15:57:53 +0900

    Create total_tvl.sql

---
+++ hyperliquid/total_tvl.sql
@@ -0,0 +1,9 @@
+select
+    b.date,
+    b.tvl as bridged_tvl,
+    u.total_tvl as unit_tvl,
+    b.tvl + u.total_tvl as total_tvl
+from query_5475036 b
+left join query_5251219 u on b.date = u.day
+where b.date < current_date
+order by b.date desc;
