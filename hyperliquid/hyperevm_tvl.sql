4970f769f2821c6e1da6f1284add1ed8d85a96fe
Youngho Lee <emfo0124@naver.com>
2025-07-16 18:10:34 +0900

    refactor: update API endpoint

--- hyperliquid/hyperevm_tvl.sql
+++ hyperliquid/hyperevm_tvl.sql
@@ -1,13 +1,29 @@
+-- with json_data as (
+--     select json_parse(http_get('https://api.llama.fi/protocol/hyperliquid')) as parsed_data
+-- ),
+
+-- tvl as (
+--     select 
+--         from_unixtime(cast(json_extract_scalar(tvl, '$.date') as bigint)) as date,
+--         cast(json_extract_scalar(tvl, '$.totalLiquidityUSD') as double) as tvl
+--     from json_data
+--     cross join unnest(cast(json_extract(parsed_data, '$.tvl') as array(json))) as t(tvl)
+-- )
+
+-- select * from tvl;
+
 with json_data as (
-    select json_parse(http_get('https://api.llama.fi/protocol/hyperliquid')) as parsed_data
+    select json_parse(http_get('https://api.llama.fi/v2/historicalChainTvl/Hyperliquid%20L1')) as parsed_data
 ),
 
 tvl as (
     select 
         from_unixtime(cast(json_extract_scalar(tvl, '$.date') as bigint)) as date,
-        cast(json_extract_scalar(tvl, '$.totalLiquidityUSD') as double) as tvl
+        cast(json_extract_scalar(tvl, '$.tvl') as double) as tvl
     from json_data
-    cross join unnest(cast(json_extract(parsed_data, '$.tvl') as array(json))) as t(tvl)
+    cross join unnest(cast(parsed_data as array(json))) as t(tvl)
 )
 
-select * from tvl;
+select * from tvl
+where date >= date '2024-12-01'
+order by date desc;
